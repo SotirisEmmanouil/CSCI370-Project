da213a094d886d83be36ee39c48b5aacb257c1d4
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-04-14 01:01:10 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        int d;
+		String room = null;
         //DAYMON's Code - Start
         //Check-in
         HotelCheckIn hotelCheckIn = new HotelCheckIn();
@@ -9,5 +11,32 @@ public class Main {
         HotelCheckOut hotelCheckOut = new HotelCheckOut();
         hotelCheckOut.performCheckOut();
         //DAYMON's Code - End
+        
+        //Sotiris' example code
+        System.out.println("Would you like some room service? Enter 1 for yes or any other digit for no");
+		Scanner scan = new Scanner(System.in);
+		d = scan.nextInt();
+		
+		if(d ==1){
+			System.out.println("Enter 1 for whiskey, 2 for wine, 3 for champagne. Enter room number as well");	
+			d = scan.nextInt();
+			room = scan.next();
+			
+		}
+		if(d==1) {
+			RoomService service = new WhiskeyService(null);
+			 service.deliver(room);
+		 System.out.println(service.serviceCost());
+		}
+		if(d==2) {
+	       RoomService service = new WineService(null);
+	      	service.deliver(room);
+	      	System.out.println(service.serviceCost());
+	   }
+		if(d==3) {
+			RoomService service = new ChampagneService(null);
+			 service.deliver(room);
+			 System.out.println(service.serviceCost());
+		}
     }
 }
