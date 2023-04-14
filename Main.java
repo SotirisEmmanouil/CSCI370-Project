93292d9adbfa115cec16a14bdfd6c0b8fc0d3d7f
tislam2021 <81173128+tislam2021@users.noreply.github.com>
2023-04-14 15:04:23 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		HotelRoom singleRoom = new HotelRoom(new DoublePrice(), new SingleBed(), new StandardTheme());
+		AddHotelRoom singleRoom = new HotelRoom(new DoublePrice(), new SingleBed(), new StandardTheme());
 		singleRoom.hotelRoomTheme();
 		singleRoom.hotelRoomBeds();
 		singleRoom.hotelRoomPrice();
