052d0eb295469511ca6b08a4ac63d836348df01b
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-04-14 01:12:28 -0400

    Create RoomService.java

---
+++ RoomService.java
@@ -0,0 +1,15 @@
+
+public abstract class RoomService {
+	
+	protected RoomService _RoomService;		// protected RoomService variable
+	
+	public RoomService(RoomService service) {	
+		//RoomService constructor
+		this._RoomService = service;
+	}
+	
+	abstract public double serviceCost();	//calculate cost
+	abstract public String servicesList();	//create a list of services provided
+	abstract public void deliver(String x);			//show the service is being provided
+
+}
