75c63ef7b298a194c953c2575a4e81f31bddbedd
Lauren Ramroop <73128614+laurenramroop@users.noreply.github.com>
2023-04-14 16:34:56 -0400

    Update HotelRequests.java

--- HotelRequests.java
+++ HotelRequests.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
 
-//add hotel rooms to list that requires cleaning
+/**
+ * @author Lauren Ramroop
+ * A class that adds and removes requests, and notifies listener 
+ */
 public class HotelRequests{
     private List<HotelRoom> hotelRoomList = new ArrayList<>();
     private EmployeePage employeePage = new EmployeePage();
-
-    //add when room is dirty 
+ 
     public void addRequests(HotelRoom hotelRoom){
         employeePage.notify(hotelRoom, true);
         hotelRoomList.add(hotelRoom);
     }
 
-    //remove once cleaned
     public void removeRequests (HotelRoom hotelRoom){
         employeePage.notify(hotelRoom, false);
         hotelRoomList.remove(hotelRoom);
