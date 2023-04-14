9c9d6c0ac6205ab0b02b7106a506ab206ff1aad0
Lauren Ramroop <73128614+laurenramroop@users.noreply.github.com>
2023-04-14 16:35:37 -0400

    Update EmployeePage.java

--- EmployeePage.java
+++ EmployeePage.java
@@ -1,8 +1,14 @@
 public class EmployeePage{
-
+    /**
+     * @author Lauren Ramroop
+     * Designed to give notification to listener (employee) based on if a request is added or removed
+     * @param hotelRoom the hotel room requesting a service
+     * @param request shows if request is being added (true) or removed (false)
+     */
     public void notify(HotelRoom hotelRoom, boolean request){
-        System.out.println(request ==true?"Room number " + hotelRoom.getRoomNumber() + " requires " + hotelRoom.getService(): "Request completed " + "for " + hotelRoom.getRoomNumber());
-        System.out.println("Requested by: "+ hotelRoom.getName());
+        System.out.println(request ==true?hotelRoom.getName() + " in room number " + hotelRoom.getRoomNumber() + " requires " + hotelRoom.getService()
+            : "Request completed for room " + hotelRoom.getRoomNumber());
 
     }
 }
+
