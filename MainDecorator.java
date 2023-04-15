2b462b6bacf805bef5e94dc390b7ef15a81ccb74
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-04-14 20:18:50 -0400

    Create MainDecorator.java

---
+++ MainDecorator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+//Author: Sotiris Emmanouil
+public class MainDecorator {
+	
+	public static void main (String [] args) {
+		int d;
+		String room = null;
+		System.out.println("Would you like some room service? Enter 1 for yes or any other digit for no");
+		Scanner scan = new Scanner(System.in);
+		d = scan.nextInt();
+		
+		if(d ==1){
+			System.out.println("Enter 1 for whiskey, 2 for wine, 3 for champagne. Enter room number as well");	
+			d = scan.nextInt();
+			room = scan.next();
+			
+		
+		if(d==1) {
+			RoomService service = new WhiskeyService(null);
+			 service.deliver(room);
+		 System.out.println(service.serviceCost());
+		}
+		if(d==2) {
+	       RoomService service = new WineService(null);
+	      	service.deliver(room);
+	      	System.out.println(service.serviceCost());
+	   }
+		if(d==3) {
+			RoomService service = new ChampagneService(null);
+			 service.deliver(room);
+			 System.out.println(service.serviceCost());
+		}
+		
+		}
+		
+		
+	}
+}
