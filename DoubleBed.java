5bbe6d06e170f57c2de4ab84c0febc808aa00aff
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-04-17 18:37:35 -0400

    Delete DoubleBed.java

--- DoubleBed.java
+++
@@ -1,9 +0,0 @@
-/* Beds for rooms with double beds and VIP suites*/
-
-public class DoubleBed implements Beds{
-	@Override
-	public String roomBeds() { 
-		return "This room has two beds.";
-	}
-
-}
