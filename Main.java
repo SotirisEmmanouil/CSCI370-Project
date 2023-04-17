a76ffaba2d08833f8aae19f6ca81ab045a282788
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-04-17 18:47:01 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -31,10 +31,6 @@ public class Main {
 		}
 		
 		}
-		AddHotelRoom singleRoom1 = new AddHotelRoom(new DoublePrice(), new SingleBed(), new StandardTheme());
-		System.out.println(singleRoom1.getHotelRoomTheme());
-		System.out.println(singleRoom1.getHotelRoomBeds());
-		System.out.println(singleRoom1.getHotelRoomPrice());
 	}
 
 }
