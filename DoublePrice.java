a9edc9c07f806f8ded23e9a42eecd2c687934159
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-04-17 18:38:10 -0400

    Delete DoublePrice.java

--- DoublePrice.java
+++
@@ -1,9 +0,0 @@
-/* The price for rooms with double beds*/
-
-public class DoublePrice implements Price {
-	@Override
-	public String roomPrice() {
-		return "This room costs $140 per night.";
-	}
-
-}
